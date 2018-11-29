275494fdec6fbd08e79cd01bcc15c874d29ed7e2
Alex <therebeloper@gmail.com>
2018-11-29 17:24:18 +0200

    Initial Commit

---
+++ FirestoreAuth/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  FirestoreAuth
+//
+//  Created by Alex Nagy on 29/11/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
